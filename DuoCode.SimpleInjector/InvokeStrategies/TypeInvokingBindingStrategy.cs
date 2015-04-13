c113934e3e7dae537ffca1552b5692292e748a98
AndersMalmgren <anders.malmgren@agero.se>
2015-04-14 00:56:00 +0200

    Performance fix

--- DuoCode.SimpleInjector/InvokeStrategies/TypeInvokingBindingStrategy.cs
+++ DuoCode.SimpleInjector/InvokeStrategies/TypeInvokingBindingStrategy.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace DuoCode.SimpleInjector.InvokeStrategies
 {
@@ -9,25 +10,27 @@ namespace DuoCode.SimpleInjector.InvokeStrategies
         private readonly Type to;
         private readonly IContainer container;
         private static readonly Type enumerableType = typeof(IEnumerable<>);
-        
+        private ConstructorInfo constructor;
+        private object[] parameters;
+
         public TypeInvokeStrategy(Type to, IContainer container)
         {
             this.to = to;
             this.container = container;
+
+            if (to.IsInterface || to.IsAbstract) throw new Exception("Can only inject concrete none abstract types");
+
+            constructor = to.GetConstructors()
+                .OrderByDescending(c => c.GetParameters().Length)
+                .First();
+
+            parameters = constructor.GetParameters()
+                .Select(p => p.ParameterType.IsGenericType && p.ParameterType.GetGenericTypeDefinition().IsAssignableFrom(enumerableType) ? container.GetAll(p.ParameterType.GetGenericArguments()[0]) : container.Get(p.ParameterType))
+                .ToArray();
         }
         
         public object Get()
         {
-            if (to.IsInterface || to.IsAbstract) throw new Exception("Can only inject concrete none abstract types");
-
-            var constructor = to.GetConstructors()
-                .OrderByDescending(c => c.GetParameters().Length)
-                .First();
-
-            var parameters = constructor.GetParameters()
-                .Select(p => p.ParameterType.IsGenericType && p.ParameterType.GetGenericTypeDefinition().IsAssignableFrom(enumerableType) ? container.GetAll(p.ParameterType.GetGenericArguments()[0]) : container.Get(p.ParameterType))
-                .ToArray();
-
             var instance = constructor.Invoke(parameters);
             return instance;
         }
