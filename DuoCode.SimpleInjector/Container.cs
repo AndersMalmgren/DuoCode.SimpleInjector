2fc70822a960d4e2e8c7339cac2d622f3c5e24be
AndersMalmgren <anders.malmgren@agero.se>
2015-10-15 19:58:27 +0200

    Updated DuoCode to 1.2 Removed temp hack after fix from DuoCode team

--- DuoCode.SimpleInjector/Container.cs
+++ DuoCode.SimpleInjector/Container.cs
@@ -9,26 +9,6 @@ namespace DuoCode.SimpleInjector
 {
     public class Container : IContainer
     {
-        static Container()
-        {
-            //Temp hack until fixed by DuoCode team
-            //http://stackoverflow.com/questions/1606797/use-of-apply-with-new-operator-is-this-possible
-
-            Js.referenceAs<Action>(@"(function() {
-    function construct(constructor, args) {
-        function F() {
-            return constructor.apply(this, args);
-        }
-        F.prototype = constructor.prototype;
-        return new F();
-    }
-
-    System.Reflection.ConstructorInfo.prototype.Invoke$3 = function(invokeAttr, binder, parameters, culture) {
-            return construct(this.jsConstructor, parameters);
-        };
-})")();
-        }
-
         private readonly Dictionary<Type, List<IInvokeStrategy>> bindings = new Dictionary<Type, List<IInvokeStrategy>>();
 
         public Container()
